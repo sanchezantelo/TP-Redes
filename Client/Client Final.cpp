544a5ca8bfebb27b97917acf8d6467961b53370a
NicoTrezza <realnikocf@gmail.com>
2020-10-11 16:48:15 -0300

    Clase Servicio - menus altaServicio y gestionarPasajes

--- Client/Client Final.cpp
+++ Client/Client Final.cpp
@@ -1,54 +1,57 @@
 #include<stdio.h>
 #include <iostream>
 #include<winsock2.h>
+#include <Servicio.h>
 using namespace std;
 
 SOCKET crearSocket();
 sockaddr_in crear_sockaddr_in(char IP[16], int PORT);
 void conexion(SOCKET sockClient, sockaddr_in sin);
+void altaServicio();
+void gestionarPasajes();
 
 int main(int arg, char** argv){
 
-char IP[] = "127.0.0.1", user[49], buffer[1000];
-const int PORT = 54321;
-SOCKET sockClient = crearSocket();
-sockaddr_in sin = crear_sockaddr_in(IP, PORT);
-conexion(sockClient, sin);
+    char IP[] = "127.0.0.1", user[49], buffer[1000];
+    const int PORT = 54321;
+    SOCKET sockClient = crearSocket();
+    sockaddr_in sin = crear_sockaddr_in(IP, PORT);
+    conexion(sockClient, sin);
 
-do {
-    system("cls");
-    printf("1-Login\n2-Salir\n\n");
-    fflush(stdin);
-    scanf("%c", buffer);
-    switch(buffer[0]){
-        case '1':
-            system("cls");
-            printf("Nombre de Usuario: ");
-            fflush(stdin);
-            fgets(user,1000,stdin);
-            send(sockClient, user, sizeof(user), 0); //envio mi Usuario al Servidor
-
-            printf("Contrase%ca: ", 164);
-            fflush(stdin);
-            memset(buffer,0,1000);
-            fgets(buffer,1000,stdin);
-            send(sockClient, buffer, sizeof(buffer), 0); //envio mi Contraseña al Servidor
-
-            //me quedo esperando para ver si ingrese bien o mal el usuario y contraseña.
-            memset(buffer,0,1000);
-            recv(sockClient, buffer, sizeof(buffer),0);
-            if(strcmp(buffer,"OK") != 0){
+    do {
+        system("cls");
+        printf("1-Login\n2-Salir\n\n");
+        fflush(stdin);
+        scanf("%c", buffer);
+        switch(buffer[0]){
+            case '1':
                 system("cls");
-                printf("Nombre de usuario o Contrase%ca incorrectos.", 164);
-                getchar();
-            }
-            break;
-        case '2': exit(1);
-        default: break;
-    }
-}while(strcmp(buffer,"OK"));
-//-----------------------------------------------------------------------------------------------------
-int opcion;
+                printf("Nombre de Usuario: ");
+                fflush(stdin);
+                fgets(user,1000,stdin);
+                send(sockClient, user, sizeof(user), 0); //envio mi Usuario al Servidor
+
+                printf("Contrase%ca: ", 164);
+                fflush(stdin);
+                memset(buffer,0,1000);
+                fgets(buffer,1000,stdin);
+                send(sockClient, buffer, sizeof(buffer), 0); //envio mi Contraseña al Servidor
+
+                //me quedo esperando para ver si ingrese bien o mal el usuario y contraseña.
+                memset(buffer,0,1000);
+                recv(sockClient, buffer, sizeof(buffer),0);
+                if(strcmp(buffer,"OK") != 0){
+                    system("cls");
+                    printf("Nombre de usuario o Contrase%ca incorrectos.", 164);
+                    getchar();
+                }
+                break;
+            case '2': exit(1);
+            default: break;
+        }
+    }while(strcmp(buffer,"OK"));
+    //-----------------------------------------------------------------------------------------------------
+    int opcion;
     do {
         system("cls");        // Para limpiar la pantalla
 
@@ -66,13 +69,13 @@ int opcion;
         switch (opcion) {
             case 1:
                 // Lista de instrucciones de la opción 1
-                //altaServicio();
+                altaServicio();
                 system("pause>nul"); // Pausa
                 break;
 
             case 2:
                 // Lista de instrucciones de la opción 2
-                //gestionarPasajes();
+                gestionarPasajes();
                 system("pause>nul"); // Pausa
                 break;
 
@@ -117,4 +120,84 @@ sockaddr_in crear_sockaddr_in(char IP[16], int PORT){
 void conexion(SOCKET sockClient, sockaddr_in sin){
     connect(sockClient,(struct sockaddr*)&sin, sizeof(sin));
     return;
+}
+
+void altaServicio() {
+    int seguir = 1;
+    char opcion;
+    int origen;
+    string fecha;
+    int turno;
+    char origen_string[3][20] = {"Buenos Aires", "Mar del Plata"};
+    char turno_string[4][20] = {"Manana", "Tarde", "Noche"};
+
+    while (seguir) {
+        do {
+            system("cls");
+            cout << "Ingresar origen (1. Buenos Aires, 2. Mar del Plata)" << endl;
+            cin >> origen;
+        } while (origen != 1 && origen != 2);
+
+        system("cls");
+        cout << "Ingresar fecha (aaaa-mm-dd)" << endl;
+        cin >> fecha;
+
+        do {
+            system("cls");
+            cout << "Ingresar turno (1. manana, 2. tarde, 3. noche)" << endl;
+            cin >> turno;
+        } while (turno < 1 || turno > 3);
+
+        system("cls");
+        cout << "Elegiste origen: " << origen_string[origen - 1] << ", fecha: " << fecha << ", turno: " << turno_string[turno - 1] << endl << "Los datos son correctos?" << endl;
+        cout << "1. Si" << endl << "2. No, ingresarlos nuevamente" << endl;
+        cin >> opcion;
+        switch (opcion) {
+            case '1': seguir = 0;/*crearServicio(origen, fecha, turno);*/ break;
+            case '2': break;
+            case '\n': break;
+            default: seguir = 0; break;
+        }
+        system("cls");
+    }
+}
+
+void gestionarPasajes() {
+    int seguir = 1;
+    char opcion;
+    int origen;
+    string fecha;
+    int turno;
+    char origen_string[3][20] = {"Vacio", "Buenos Aires", "Mar del Plata"};
+    char turno_string[4][20] = {"Vacio", "Manana", "Tarde", "Noche"};
+
+    while (seguir) {
+        do {
+            system("cls");
+            cout << "Buscar por origen (1. Buenos Aires, 2. Mar del Plata). [Si no desea buscar por origen ingrese 0]" << endl;
+            cin >> origen;
+        } while (origen < 0 || origen > 2);
+
+        system("cls");
+        cout << "Buscar por fecha (aaaa-mm-dd). [Si no desea buscar por fecha ingrese 0]" << endl;
+        cin >> fecha;
+
+        do {
+            system("cls");
+            cout << "Buscar por turno (1. manana, 2. tarde, 3. noche). [Si no desea buscar por turno ingrese 0]" << endl;
+            cin >> turno;
+        } while (turno < 0 || turno > 3);
+
+        system("cls");
+        cout << "Elegiste origen: " << origen_string[origen] << ", fecha: " << fecha << ", turno: " << turno_string[turno] << endl << "Los datos son correctos?" << endl;
+        cout << "1. Si" << endl << "2. No, ingresarlos nuevamente" << endl;
+        cin >> opcion;
+        switch (opcion) {
+            case '1': seguir = 0;/*crearServicio(origen, fecha, turno);*/ break;
+            case '2': break;
+            case '\n': break;
+            default: seguir = 0; break;
+        }
+        system("cls");
+    }
 }
